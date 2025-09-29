018ff74b274e102812af1055c853e77c53f85172
nishantAIandML <you@example.com>
2025-09-29 15:30:55 +0530

    ls commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
